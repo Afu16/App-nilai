8b9baf64f70feb0683d7e58957d3c8682a573bd5
Afu <afumanan07@gmail.com>
2025-02-25 21:39:20 -0800

    Update guru.php

--- dashboard/guru.php
+++ dashboard/guru.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 'guru') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Guru</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href=".css">
 </head>
 <body>
 
