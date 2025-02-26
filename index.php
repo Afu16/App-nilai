e7acfbddf8bbaca0e3a71b8be3139db3371e284e
Afu <afumanan07@gmail.com>
2025-02-25 21:40:19 -0800

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,7 @@ if (isset($_SESSION['role'])) {
     <meta charset="UTF-8">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="tyle.css">
+ <link rel="stylesheet" href=".css">
     <title>Login</title>
 </head>
 <body>
