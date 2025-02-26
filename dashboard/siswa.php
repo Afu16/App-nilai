101f7462ae68bc8cf901b00dc9ec5614e360a458
Afu <afumanan07@gmail.com>
2025-02-25 21:39:43 -0800

    Update siswa.php

--- dashboard/siswa.php
+++ dashboard/siswa.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 'siswa') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Siswa</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
+ <link rel="stylesheet" href=".css">
 </head>
 <body>
 
