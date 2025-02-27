6575fc88576904a2f218eb735583c94a84b56896
afu manan <afumanan07@gmail.com>
2025-02-27 09:25:16 -0800

    versi 1.6

---
+++ mapel/mapelsiswa.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MAPEL</title>
+ <link rel="stylesheet" href=".css">
+</head>
+<body>
+<header>
+    <h1>Mapel</h1>
+    <nav>
+        <ul>
+        <li><a style="text-decoration: none;" href="../dashboard/guru.php">Beranda</a></li>
+        <li><a style="text-decoration: none;" href="../logout.php">Logout</a></li>
+        </ul>
+    </nav>
+</header>
+<table border="1">
+<th>MAPEL</th>
+<tr>
+    <td>PAI</td>
+</tr>
+<tr>
+    <td>B.INDO</td>
+</tr>
+<tr>
+    <td>B.INGGRIS</td>
+</tr>
+<tr>
+    <td>MATEMATIKA</td>
+</tr>
+<tr>
+    <td>KEJURUAN</td>
+</tr>
+<tr>
+    <td>PPKN</td>
+</tr>
+
+</table>
